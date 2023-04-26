7f9bf020c4103558eda2749d2cee147c609a042b
mrperrytpx <tomislav.siprak1996@gmail.com>
2023-04-26 18:50:29 +0200

    Sections done maybe idno

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ const sections = document.querySelectorAll("section");
 
 const options = {
   rootMargin: "-100px 0px",
-  threshold: 0.33,
+  threshold: 0.333,
 };
 
 const observer = new IntersectionObserver((entries, _observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       const bgColor = entry.target.dataset.bg;
-      const currColor = [...document.body.classList].find((x) => x.includes("bg-"));
+      const currColor = [...document.body.classList].find((x) =>
+        x.includes("bg-")
+      );
       if (currColor) document.body.classList.remove(currColor);
       document.body.classList.add(bgColor);
     }
